1cf568c8b28a0ecd1d6f19efe56999e35cf6cb59
Jeremy Stucki <stucki.jeremy@gmail.com>
2016-04-29 08:27:37 +0200

    added new api endpoint

--- server/api.py
+++ server/api.py
@@ -1,6 +1,9 @@
+import json
+import traceback
+
 import flask
 import pymongo
-from flask import Flask
+from flask import Flask, Response
 
 app = Flask(__name__)
 
@@ -18,9 +21,27 @@ def menus_for_location(location):
                                           .find({'location': location}, {'_id': 0, 'location': 0}))})
 
 
+@app.route('/api/v1/coop/menus/<location>/<timestamp>')
+def menus_for_location_and_timestamp(location, timestamp):
+    try:
+        int(timestamp)
+    except ValueError:
+        return Response(
+            response=json.dumps({'results': [], 'error': 'timestamp is not an int'}),
+            mimetype='application/json',
+            status=400
+        )
+
+    return flask.jsonify(
+        {'results': list(db.get_collection('menus')
+                         .find({'location': location, 'timestamp': int(timestamp)},
+                               {'_id': 0, 'location': 0}))})
+
+
 @app.errorhandler(404)
 def not_found(_):
     return flask.jsonify({'error': 'page not found'}), 404
+
 
 if __name__ == '__main__':
     app.run('localhost', 8888)
